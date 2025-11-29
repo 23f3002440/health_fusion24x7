362ca1cc3b75abbc556f5f60da0892f4fe1cd425
23f3002440 <23f3002440@ds.study.iitm.ac.in>
2025-11-29 09:01:45 +0530

    Milestone-HMS DB-Relationship

---
+++ controller/models.py
@@ -0,0 +1,49 @@
+from controller.database import db
+
+class User(db.Model):
+    
+    # __tablename__ = 'user'
+    
+    user_id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    user_name = db.Column(db.String(50), nullable=False)
+    user_email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+
+    roles = db.relationship('Role', secondary = 'user_role', backref = 'users', lazy = True)
+    customer_details = db.relationship('Customer', backref='user', lazy=True, uselist=False)
+    store_manager_details = db.relationship('StoreManager', backref='user', lazy=True, uselist=False)
+    
+    
+# store_manager = StoreManager(qualification='Bsc')
+# user = User(user_email, password, user_name, store_manager_details=store_manager)
+# db.session.commit()
+    
+class Role(db.Model):
+    
+    #__tablename__ = 'role'
+    
+    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    name = db.Column(db.String(50), nullable=False)
+    
+class UserRole(db.Model):
+    
+    # __tablename__ = 'user_role'
+    
+    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
+    
+class StoreManager(db.Model):
+    
+    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    qualification = db.Column(db.String(100), nullable=False)
+    
+class Customer(db.Model):
+    
+    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    address = db.Column(db.String(50), nullable=False)
+    prefered_mode_of_payment = db.Column(db.String(50), nullable=False)
+    phone_number= db.Column(db.String(50), nullable=False)
+    
---
+++ main.py
@@ -0,0 +1,71 @@
+from flask import Flask, render_template
+from controller.database import db
+from controller.config import config
+from controller.models import * 
+
+app = Flask(__name__, template_folder='templates', static_folder='static') # create the app
+app.config.from_object(config) #load config from config.py
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.sqlite3" # sqlite database in app
+
+db.init_app(app) #initialize app with extension
+
+with app.app_context():
+    db.create_all()  # create database tables for our data models
+    
+    admin_role = Role.query.filter_by(name='admin').first()
+    if not admin_role:
+        admin_role = Role(name='admin')
+        db.session.add(admin_role)
+        
+    cust_role = Role.query.filter_by(name='customer').first()
+    if not cust_role:
+        cust_role = Role(name='customer')
+        db.session.add(cust_role)
+        
+    manager_role = Role.query.filter_by(name='manager').first()
+    if not manager_role:
+        manager_role = Role(name='manager')
+        db.session.add(manager_role)
+    
+    # admin_user = User.query.filter_by(user_email='admin@gmail.com').first()
+    # if not admin_user:
+    #     admin_user = User(
+    #         user_email = "admin@gmail.com",
+    #         password = "1234567890",
+    #         user_name = "Super Admin"
+    #     )
+    #     db.session.add(admin_user)
+    #     # print(admin_user.id)
+    #     admin_user_details = User.query.filter_by(user_email = "admin@gmail.com").first()
+    #     admin_role = Role.query.filter_by(name='admin').first()
+        
+    #     user_id = admin_user_details.user_id
+    #     role_id = admin_role.id
+        
+    #     user_role = UserRole(user_id=user_id, role_id=role_id)
+    #     db.session.add(user_role)
+    
+    admin_user = User.query.filter_by(user_email = 'admin@gmail.com').first()
+    if not admin_user:
+        admin_role = Role.query.filter_by(name='admin').first()
+        admin_user= User(
+            user_email = "admin@gmail.com",
+            password = "1234567890",
+            user_name = "Super Admin",
+            roles = [admin_role]
+        )
+        db.session.add(admin_user)
+        
+    db.session.commit()
+
+@app.route('/')
+def index():
+    return render_template('hello.html')
+
+@app.route('/about')
+def about():
+    return "About page"
+
+
+if __name__ == "__main__":
+    app.run()
